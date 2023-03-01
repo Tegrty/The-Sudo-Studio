afe894fac22a32b8775e7214253d3a0aefa18fae
Mario Repas <mario.repas63@gmail.com>
2023-03-01 15:33:29 -0500

    debugging new blog form submit

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,19 +5,22 @@ const newBlogHandler = async (event) => {
   const content = document.querySelector("#blog-content").value.trim();
   if (title && content) {
     // Send a POST request to the API endpoint
-    const response = await fetch(`/api/blogs`, {
+    const response = await fetch(`/api/blogs/`, {
       method: "POST",
       body: JSON.stringify({ title, content }),
       headers: { "Content-Type": "application/json" },
     });
+    console.log(response);
     if (response.ok) {
       // If successful, redirect the browser to the dashboard page
       document.location.replace("/dashboard");
+      console.log("New blog created");
     } else {
       alert(response.statusText);
     }
   }
 };
+console.log("adding event listener");
 document
   .querySelector(".new-blog-form")
   .addEventListener("submit", newBlogHandler);
